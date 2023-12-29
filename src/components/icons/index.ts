c080ab8ac86f967059cb8055959c6d25f85116fe
Vicente Rivera <vicenterivera225@gmail.com>
2023-12-28 22:31:05 -0300

    fix: icon import

---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+import CodeIcon from "./Code.astro";
+import LinkedinIcon from "./Linkedin.astro";
+import GithubIcon from "./Github.astro";
+import BriefcaseIcon from "./Briefcase.astro";
+import NextJSIcon from "./NextJS.astro";
+
+export { CodeIcon, LinkedinIcon, GithubIcon, BriefcaseIcon, NextJSIcon }
