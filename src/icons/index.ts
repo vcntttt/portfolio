bfa97e90aea74d568956509bf2328b4f1b541918
Vicente Rivera <vicenterivera225@gmail.com>
2023-12-28 22:57:14 -0300

    imports relativos

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,5 +3,6 @@ import LinkedinIcon from "./Linkedin.astro";
 import GithubIcon from "./Github.astro";
 import BriefcaseIcon from "./Briefcase.astro";
 import NextJSIcon from "./NextJS.astro";
+import TailwindIcon from "./TailwindCSS.astro"
 
-export { CodeIcon, LinkedinIcon, GithubIcon, BriefcaseIcon, NextJSIcon }
+export { CodeIcon, LinkedinIcon, GithubIcon, BriefcaseIcon, NextJSIcon, TailwindIcon }
