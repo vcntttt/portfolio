b13a416312dbc5aabadab9762fefb24734e0db36
Vicente Rivera <vicenterivera225@gmail.com>
2024-12-21 21:10:47 -0300

    avance en proyectos + acerca de mi

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -4,5 +4,6 @@ import GithubIcon from "./Github.astro";
 import BriefcaseIcon from "./Briefcase.astro";
 import NextJSIcon from "./NextJS.astro";
 import TailwindIcon from "./TailwindCSS.astro"
+import UserIcon from "./User.astro";
 
-export { CodeIcon, LinkedinIcon, GithubIcon, BriefcaseIcon, NextJSIcon, TailwindIcon }
+export { CodeIcon, LinkedinIcon, GithubIcon, BriefcaseIcon, NextJSIcon, TailwindIcon, UserIcon };
