beef860b5989d55f2a0496e3c9c990cc9a866d64
Vicente Rivera <vicenterivera225@gmail.com>
2024-12-23 16:57:02 -0300

    migracion a astro-icon

--- src/icons/index.ts
+++
@@ -1,9 +0,0 @@
-import CodeIcon from "./Code.astro";
-import LinkedinIcon from "./Linkedin.astro";
-import GithubIcon from "./Github.astro";
-import BriefcaseIcon from "./Briefcase.astro";
-import NextJSIcon from "./NextJS.astro";
-import TailwindIcon from "./TailwindCSS.astro"
-import UserIcon from "./User.astro";
-import RocketIcon from "./Rocket.astro";
-export { CodeIcon, LinkedinIcon, GithubIcon, BriefcaseIcon, NextJSIcon, TailwindIcon, UserIcon, RocketIcon };
