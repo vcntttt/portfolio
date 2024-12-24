b38229b4237847f224e946740ecc77fff9d4f495
Vicente Rivera <vicenterivera225@gmail.com>
2024-12-24 15:01:59 -0300

    mejor descripcion en shortener

--- src/components/Projects/tags.ts
+++ src/components/Projects/tags.ts
@@ -13,5 +13,20 @@ export const TAGS = {
     name: "React",
     class: "bg-green-500 text-white",
     icon: "React",
+  },
+  TYPESCRIPT: {
+    name: "TypeScript",
+    class: "bg-blue-950 text-white",
+    icon: "TypeScript",
+  },
+  PRISMA: {
+    name: "Prisma",
+    class: "bg-slate-900 text-white",
+    icon: "Prisma",
+  },
+  SHADCN: {
+    name: "ShadCN",
+    class: "bg-black text-white",
+    icon: "shadcn",
   }
 }
