4011ade331e756db184604f89a39fd9aea72ad49
Vicente Rivera <vrivera.dev@gmail.com>
2025-01-04 16:28:27 -0300

    certificados!

--- src/components/Cursos/cursos.ts
+++ src/components/Cursos/cursos.ts
@@ -1,31 +1,38 @@
+import type { ImageMetadata } from "astro";
+import certificadoAstro from "@/assets/certificado-astro.jpg";
+import certificadoTanstack from "@/assets/certificado-tanstack.png";
+
+
 export interface Curso {
   title: string;
-  author: string;
+  instructor: string;
   plataforma: string;
-  image:string;
-  finished: boolean;
+  image: ImageMetadata;
+  icon:string;
+  link: string
 }
 
 export const cursos: Curso[] = [
   {
     title: "Astro",
-    author: "Fernando Herrera",
+    instructor: "Fernando Herrera",
     plataforma: "Udemy",
-    image: "Astro",
-    finished: true,
-  },
-  {
-    title: "Next.js",
-    author: "Fernando Herrera",
-    plataforma: "Udemy",
-    image: "NextJS",
-    finished: false,
+    icon: "Astro",
+    image: certificadoAstro,
+    link: "https://www.udemy.com/certificate/UC-737be89c-1698-4aac-9c25-8d9c03ded0fa/"
   },
   {
     title: "TanStack Query",
-    author: "Fernando Herrera",
+    instructor: "Fernando Herrera",
     plataforma: "DevTalles",
-    image: "tanstack",
-    finished: true,
+    icon: "tanstack",
+    image: certificadoTanstack,
+    link: "https://cursos.devtalles.com/certificates/lsef9snnrv"
   },
+  // {
+  //   title: "Next.js",
+  //   instructor: "Fernando Herrera",
+  //   plataforma: "Udemy",
+  //   icon: "NextJS",
+  // },
 ];
