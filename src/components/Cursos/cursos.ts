6b04624edeef95ce7a3612f5894b773b938d16ab
Vicente Rivera <vrivera.dev@gmail.com>
2025-01-16 19:11:25 -0300

    nuevo curso completado: nextjs

--- src/components/Cursos/cursos.ts
+++ src/components/Cursos/cursos.ts
@@ -1,24 +1,25 @@
-import type { ImageMetadata } from "astro";
-import certificadoAstro from "@/assets/certificado-astro.jpg";
-import certificadoTanstack from "@/assets/certificado-tanstack.png";
-
 
 export interface Curso {
   title: string;
   instructor: string;
   plataforma: string;
-  image: ImageMetadata;
   icon:string;
   link: string
 }
 
 export const cursos: Curso[] = [
   {
+    title: "Next.js",
+    instructor: "Fernando Herrera",
+    plataforma: "Udemy",
+    icon: "NextJS",
+    link: "https://www.udemy.com/certificate/UC-9e0f0c27-e4b1-44f0-8dca-b5c572e8f443/"
+  },
+  {
     title: "Astro",
     instructor: "Fernando Herrera",
     plataforma: "Udemy",
     icon: "Astro",
-    image: certificadoAstro,
     link: "https://www.udemy.com/certificate/UC-737be89c-1698-4aac-9c25-8d9c03ded0fa/"
   },
   {
@@ -26,13 +27,6 @@ export const cursos: Curso[] = [
     instructor: "Fernando Herrera",
     plataforma: "DevTalles",
     icon: "tanstack",
-    image: certificadoTanstack,
     link: "https://cursos.devtalles.com/certificates/lsef9snnrv"
   },
-  // {
-  //   title: "Next.js",
-  //   instructor: "Fernando Herrera",
-  //   plataforma: "Udemy",
-  //   icon: "NextJS",
-  // },
 ];
