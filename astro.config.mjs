29cdb17c329fbf74b10eecf9ead872b69019a002
Vicente Rivera <vrivera.dev@gmail.com>
2025-01-23 17:32:07 -0300

    dominio .is-a.dev!

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: 'https://vcntttt.github.io',
-  base: "portfolio"
+  site: 'https://vrivera.is-a.dev',
 });
