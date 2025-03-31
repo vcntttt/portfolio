abb0998ab8d83b251fb8247b380b408bb563f36b
Vicente Rivera <vrivera.dev@gmail.com>
2025-03-31 19:21:17 -0300

    remove openpanel

--- src/op.ts
+++
@@ -1,8 +0,0 @@
-import { OpenPanel } from '@openpanel/web';
- 
-export const op = new OpenPanel({
-  clientId: "d0993031-4ef7-4818-8801-90224ef450c0",
-  trackScreenViews: true,
-  trackOutgoingLinks: true,
-  trackAttributes: true,
-});
